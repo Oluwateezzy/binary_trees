5eb08fa39f12719201d46ce71903589b2b0201bd
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-12 09:51:01 -0700

    updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_height - function that measures the height of a binary
  * @tree: pointer to root
- * Return: signed int 
+ * Return: signed int
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
