3dc9a81dd4a8b2e5d3687eceb8885f3f1dc1f2d8
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-12 06:46:47 -0700

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,18 +2,20 @@
 /**
  * binary_tree_insert_left - Write a function that inserts at left node
  * @parent: pointer to node
- * @value; value to be inserted
+ * @value: value to be inserted
  * Return: pointer to node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent == NULL) return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 
-	if (new_node == NULL) return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
 	if (parent->left != NULL)
 	{
@@ -21,5 +23,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 	}
 	parent->left = new_node;
-	return new_node;
+	return (new_node);
 }
